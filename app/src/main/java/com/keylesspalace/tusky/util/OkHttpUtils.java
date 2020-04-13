f24c615cfb26f729cad5343711495454b62f751e
Unknown <johanw@xs4all.nl>
2020-04-13 02:33:28 +0200

    Removed censorship. Added a Gab branding. Split output into abi specific apk's.

--- app/src/main/java/com/keylesspalace/tusky/util/OkHttpUtils.java
+++ app/src/main/java/com/keylesspalace/tusky/util/OkHttpUtils.java
@@ -76,7 +76,7 @@ public class OkHttpUtils {
         return chain -> {
             Request originalRequest = chain.request();
             Request requestWithUserAgent = originalRequest.newBuilder()
-                    .header("User-Agent", "Tusky/"+ BuildConfig.VERSION_NAME+" Android/"+Build.VERSION.RELEASE)
+                    .header("User-Agent", "Gab/"+ BuildConfig.VERSION_NAME+" Android/"+Build.VERSION.RELEASE) // JW: change user agent name
                     .build();
             return chain.proceed(requestWithUserAgent);
         };
