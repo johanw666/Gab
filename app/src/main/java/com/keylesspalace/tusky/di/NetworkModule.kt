2ef3700caf3af71da181e3ab933d1a45b495e7d2
Unknown <johanw@xs4all.nl>
2021-02-24 21:22:30 +0100

    Removed censorship. Added a Gab branding. Split output into abi specific apk's.

--- app/src/main/java/com/keylesspalace/tusky/di/NetworkModule.kt
+++ app/src/main/java/com/keylesspalace/tusky/di/NetworkModule.kt
@@ -78,7 +78,7 @@ class NetworkModule {
                 val requestWithUserAgent = chain.request().newBuilder()
                     .header(
                         "User-Agent",
-                        "Tusky/${BuildConfig.VERSION_NAME} Android/${Build.VERSION.RELEASE} OkHttp/${OkHttp.VERSION}"
+                        "Gab/${BuildConfig.VERSION_NAME} Android/${Build.VERSION.RELEASE} OkHttp/${OkHttp.VERSION}" // JW: change user agent name
                     )
                     .build()
                 chain.proceed(requestWithUserAgent)
