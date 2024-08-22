4c916d200192881f61760f6dbc251a0a2cf60d6c
Johan <johanw@xs4all.nl>
2024-08-22 03:41:42 +0200

    Removed censorship. Added a Gab branding. Split output into abi specific apk's.

--- app/src/main/java/com/keylesspalace/tusky/AboutActivity.kt
+++ app/src/main/java/com/keylesspalace/tusky/AboutActivity.kt
@@ -83,6 +83,11 @@ class AboutActivity : BottomSheetActivity() {
             viewUrl(BuildConfig.SUPPORT_ACCOUNT_URL)
         }
 
+        // JW: added
+        if (BuildConfig.SUPPORT_ACCOUNT_URL.isBlank()) {
+            binding.tuskyProfileButton.hide()
+        }
+
         binding.aboutLicensesButton.setOnClickListener {
             startActivityWithSlideInAnimation(Intent(this, LicenseActivity::class.java))
         }
